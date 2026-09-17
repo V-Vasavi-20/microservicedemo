4d45efe327dda67f59f0b672bb5b931cafca1c3d
V-Vasavi-20 <vasavi@example.com>
2026-09-17 10:20:46 +0000

    Fix compile error: use spring.autoconfigure.exclude property instead of unsupported excludeAutoConfiguration attribute

--- employee-service/src/test/java/com/crm/employee/EmployeeServiceApplicationTests.java
+++ employee-service/src/test/java/com/crm/employee/EmployeeServiceApplicationTests.java
@@ -1,10 +1,13 @@
 package com.crm.employee;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(excludeAutoConfiguration = DataSourceAutoConfiguration.class)
+@SpringBootTest
+@TestPropertySource(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration"
+})
 class EmployeeServiceApplicationTests {
 
     @Test
